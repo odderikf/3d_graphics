efe7fe594cf02248663dd27ec871fd394c563559
odderikf <odderikf@stud.ntnu.no>
2019-09-10 00:00:15 +0200

    transferring to desktop

--- rubiks_cube/main.cpp
+++ rubiks_cube/main.cpp
@@ -7,6 +7,7 @@
 double t = 0;
 
 const double _PERSPECTIVE = 60.0;
+const double _CAM_DISTANCE = 8;
 const double _NEAR = 2;
 const double _FAR = 15.;
 
@@ -27,20 +28,32 @@ std::array<double, 4> COLOR_L = {1.0, 0.5, 0.0, 1.0}; //orange
 std::array<double, 4> COLOR_R = {1.0, 0.0, 0.0, 1.0}; //red
 std::array<double, 4> COLOR_INTERNAL = {0.0, 0.0, 0.0, 1.0}; //black
 
-class corner{
-private:
+std::array<double, 4> ROTATION3d_NONE = {0.0, 0.0, 1.0, 0.0};
+std::array<double, 4> ROTATION3d_U = {-90.0, 0.0, 1.0, 0.0};
+std::array<double, 4> ROTATION3d_U2 = {-180.0, 0.0, 1.0, 0.0};
+std::array<double, 4> ROTATION3d_U3 = {-270.0, 0.0, 1.0, 0.0};
+std::array<double, 4> ROTATION3d_Z2 = {180.0, 0.0, 0.0, 1.0};
+std::array<double, 4> ROTATION3d_Z2_U = {180.0, 0.0, 0.0, 1.0};
 
+class corner{
 private:
     std::array<double, 4> _color_U = {0,0,0,1};
     std::array<double, 4> _color_B = {0,0,0,1};
     std::array<double, 4> _color_R = {0,0,0,1};
+    std::array<double, 4> _prerotate = {0,0,1,0};
     int rotation = 0;
+
 public:
     corner() = default;
 
     corner(std::array<std::array<double, 4>, 3> UBRR) :
     _color_U(UBRR[0]), _color_B(UBRR[1]), _color_R(UBRR[2])
     {}
+
+    corner(std::array<std::array<double, 4>, 3> UBRR, std::array<double, 4> prerotate) :
+            _color_U(UBRR[0]), _color_B(UBRR[1]), _color_R(UBRR[2]), _prerotate(prerotate)
+    {}
+
 
     void rotate(int amount){
         rotation += amount + 3;
@@ -52,6 +65,8 @@ public:
 
         if(rotation == 1) glRotated(-120, 1, 1, -1);
         if(rotation == 2) glRotated(-240, 1, 1, -1);
+
+        glRotated(_prerotate[0], _prerotate[1], _prerotate[2], _prerotate[3]);
         glColor4dv(_color_U.data());
         glBegin(GL_TRIANGLE_STRIP);
             glNormal3d(0, 1, 0);
@@ -102,17 +117,15 @@ std::array<corner, 8> corners;
 
 std::array<corner *, 8> corner_positions;
 
-void display()
-{
+void display() {
     glClearColor(0.5, 0.5, 0.5, 1.f);
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
 
-    glTranslated(0, 0, -8.);
+    glTranslated(0, 0, -_CAM_DISTANCE);
     glRotated(t, 1, 1, 0);
 
     corner_positions[0]->display();
-    glRotated(-90, 0, 1, 0);
     corner_positions[1]->display();
     glRotated(-90, 0, 1, 0);
     corner_positions[2]->display();
@@ -208,18 +221,22 @@ void reshape(int w, int h){
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glEnable(GL_DEPTH_TEST);
+    //glEnable(GL_LIGHTING);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
     glDepthFunc(GL_LEQUAL);
     //glEnable(GL_BLEND);
     //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glViewport(0, 0, w, h);
     gluPerspective(_PERSPECTIVE, ratio, _NEAR, _FAR);
-    gluLookAt(0, 0, 0, 0, 0, -1.0, 0, 1, 0);
+    gluLookAt(0, 0, 0, 0, 0, -_CAM_DISTANCE, 0, 1, 0);
 
     display();
 }
+
 void init_cube(){
-    corners[0] = corner({COLOR_U, COLOR_B, COLOR_R});
-    corners[1] = corner({COLOR_U, COLOR_R, COLOR_F});
+    corners[0] = corner({COLOR_U, COLOR_B, COLOR_R}, ROTATION3d_NONE);
+    corners[1] = corner({COLOR_U, COLOR_R, COLOR_F}, ROTATION3d_U);
     corners[2] = corner({COLOR_U, COLOR_F, COLOR_L});
     corners[3] = corner({COLOR_U, COLOR_L, COLOR_B});
 
@@ -233,8 +250,7 @@ void init_cube(){
     }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize (800, 800);
