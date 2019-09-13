68d74f801c2c1eced22f21f9f9b28235a6227975
odderikf <odderikf@stud.ntnu.no>
2019-09-13 03:02:21 +0200

    added camera rotation and actual animation of moves. However, something's very wrong with the F, B, L and R moves. Probably some oversight with matrix order of operations or something

--- rubiks_cube/main.cpp
+++ rubiks_cube/main.cpp
@@ -6,17 +6,23 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/constants.hpp>
+#include <cmath>
 
 #include <initializer_list>
 #include <array>
 #include <iostream>
 
 double t = 0;
+double camera_ang_y = 0;
+double camera_ang_xz = 0;
+bool is_animating = false;
 
 const double _PERSPECTIVE = 60.0;
 const double _CAM_DISTANCE = 8;
+const double _CAM_SPEED = 0.1;
 const double _NEAR = 2;
 const double _FAR = 15.;
+const double _ANIM_STEPS = 100;
 
 enum face{
     U = 0,
@@ -35,28 +41,29 @@ std::array<double, 4> COLOR_L = {1.0, 0.5, 0.0, 1.0}; //orange
 std::array<double, 4> COLOR_R = {1.0, 0.0, 0.0, 1.0}; //red
 std::array<double, 4> COLOR_INTERNAL = {0.0, 0.0, 0.0, 1.0}; //black
 
-glm::dmat4 &turn_face_mat(glm::dmat4 &mat, const face &f, const double &angle = -glm::half_pi<double>()){
-    glm::dvec3 axis;
-    switch (f){
-        case U: axis = {0, 1, 0}; break;
-        case D: axis = {0, -1, 0}; break;
-        case F: axis = {0, 0, 1}; break;
-        case B: axis = {0, 0, -1}; break;
-        case L: axis = {-1, 0, 0}; break;
-        case R: axis = {1, 0, 0}; break;
-        default: return mat;
-    }
+glm::dvec3 move_axes[6] = {
+        {0, 1, 0}, //U
+        {0, -1, 0}, //D
+        {0, 0, 1}, //F
+        {0, 0, -1}, //B
+        {-1, 0, 0}, //L
+        {1, 0, 0}, //R
+};
 
-    mat = glm::rotate(mat, angle, axis);
+glm::dmat4 &turn_face_mat(glm::dmat4 &mat, const face &f, const double &angle = -glm::half_pi<double>()){
+    mat = glm::rotate(mat, angle, move_axes[f]);
     return mat;
 }
+
 class corner{
 private:
     std::array<double, 4> _color_U = {0,0,0,1};
     std::array<double, 4> _color_B = {0,0,0,1};
     std::array<double, 4> _color_R = {0,0,0,1};
+    bool piece_animating = false;
     glm::dmat4 _model = glm::dmat4(1.);
-    int rotation = 0;
+    glm::dmat4 animation_target = _model;
+    glm::dvec3 animation_axes = {0,0,0};
 
 public:
     corner() = default;
@@ -75,83 +82,103 @@ public:
             _color_U(UBRR[0]), _color_B(UBRR[1]), _color_R(UBRR[2]), _model(model)
     {}
 
-
-    void rotate(int amount){
-        rotation += amount + 3;
-        rotation %= 3;
+    void animate(glm::dvec3 axes){
+        piece_animating = true;
+        animation_target = glm::rotate(_model, glm::half_pi<double>(), axes);
+        animation_axes = axes;
     }
 
     void display(){
+        auto curr_model = _model;
+        if(piece_animating) {
+            if(is_animating){
+                curr_model = glm::rotate(_model, t * glm::half_pi<double>(), animation_axes);
+            }
+            else {
+                _model = animation_target;
+                curr_model = _model;
+                piece_animating = false;
+            }
+        }
+
         glPushMatrix();
+            glMultMatrixd( glm::value_ptr( curr_model ) );
 
-        glMultMatrixd( glm::value_ptr( _model ) );
-        if(rotation == 1) glRotated(-120, 1, 1, -1);
-        if(rotation == 2) glRotated(-240, 1, 1, -1);
+            glColor4dv(_color_U.data());
+            glBegin(GL_TRIANGLE_STRIP);
+                glNormal3d(0, 1, 0);
+                glVertex3d(1.5, 1.5, -1.5);
+                glVertex3d(1.5, 1.5, -0.5);
+                glVertex3d(0.5, 1.5, -1.5);
+                glVertex3d(0.5, 1.5, -0.5);
+            glEnd();
 
+            glColor4dv(_color_B.data());
+            glBegin(GL_TRIANGLE_STRIP);
+                glNormal3d(0, 0, -1);
+                glVertex3d(1.5, 1.5, -1.5);
+                glVertex3d(0.5, 1.5, -1.5);
+                glVertex3d(1.5, 0.5, -1.5);
+                glVertex3d(0.5, 0.5, -1.5);
+            glEnd();
 
-        glColor4dv(_color_U.data());
-        glBegin(GL_TRIANGLE_STRIP);
-            glNormal3d(0, 1, 0);
-            glVertex3d(1.5, 1.5, -1.5);
-            glVertex3d(1.5, 1.5, -0.5);
-            glVertex3d(0.5, 1.5, -1.5);
-            glVertex3d(0.5, 1.5, -0.5);
-        glEnd();
+            glColor4dv(_color_R.data());
+            glBegin(GL_TRIANGLE_STRIP);
+                glNormal3d(1, 0, 0);
+                glVertex3d(1.5, 1.5, -1.5);
+                glVertex3d(1.5, 0.5, -1.5);
+                glVertex3d(1.5, 1.5, -0.5);
+                glVertex3d(1.5, 0.5, -0.5);
+            glEnd();
 
-        glColor4dv(_color_B.data());
-        glBegin(GL_TRIANGLE_STRIP);
-            glNormal3d(0, 0, -1);
-            glVertex3d(1.5, 1.5, -1.5);
-            glVertex3d(0.5, 1.5, -1.5);
-            glVertex3d(1.5, 0.5, -1.5);
-            glVertex3d(0.5, 0.5, -1.5);
-        glEnd();
-
-        glColor4dv(_color_R.data());
-        glBegin(GL_TRIANGLE_STRIP);
-            glNormal3d(1, 0, 0);
-            glVertex3d(1.5, 1.5, -1.5);
-            glVertex3d(1.5, 0.5, -1.5);
-            glVertex3d(1.5, 1.5, -0.5);
-            glVertex3d(1.5, 0.5, -0.5);
-        glEnd();
-
-        glColor4dv(COLOR_INTERNAL.data());
-        glBegin(GL_TRIANGLE_FAN);
-            glNormal3d(-1, 0, 0);
-            glVertex3d(0.5, 0.5, -0.5);
-            glVertex3d(0.5, 0.5, -1.5);
-            glVertex3d(0.5, 1.5, -1.5);
-            glVertex3d(0.5, 1.5, -0.5);
-            glNormal3d(0, 0, 1);
-            glVertex3d(1.5, 1.5, -0.5);
-            glVertex3d(1.5, 0.5, -0.5);
-            glNormal3d(0, -1, 0);
-            glVertex3d(1.5, 0.5, -1.5);
-            glVertex3d(0.5, 0.5, -1.5);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex3d(0, 0, 0);
-            glVertex3d(1.5, 1.5, -1.5);
-        glEnd();
+            glColor4dv(COLOR_INTERNAL.data());
+            glBegin(GL_TRIANGLE_FAN);
+                glNormal3d(-1, 0, 0);
+                glVertex3d(0.5, 0.5, -0.5);
+                glVertex3d(0.5, 0.5, -1.5);
+                glVertex3d(0.5, 1.5, -1.5);
+                glVertex3d(0.5, 1.5, -0.5);
+                glNormal3d(0, 0, 1);
+                glVertex3d(1.5, 1.5, -0.5);
+                glVertex3d(1.5, 0.5, -0.5);
+                glNormal3d(0, -1, 0);
+                glVertex3d(1.5, 0.5, -1.5);
+                glVertex3d(0.5, 0.5, -1.5);
+            glEnd();
+            glBegin(GL_LINES);
+                glVertex3d(0, 0, 0);
+                glVertex3d(1.5, 1.5, -1.5);
+            glEnd();
         glPopMatrix();
     }
 };
 
-std::array<corner, 8> corners;
+std::array<corner *, 8> corners;
 
 std::array<corner *, 8> corner_positions;
 
 void display() {
+
     glClearColor(0.5, 0.5, 0.5, 1.f);
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
 
-    glTranslated(0, 0, -_CAM_DISTANCE);
-    glRotated(t, 1, 1, 0);
+    if(is_animating) {
+        t += 1./_ANIM_STEPS;
+    }
+    if(t >= 1){
+        t = 0;
+        is_animating = false;
+    }
+    glm::dvec4 cam_pos = {0, _CAM_DISTANCE*std::sin(camera_ang_y), _CAM_DISTANCE*std::cos(camera_ang_y), 1};
+    glm::dmat4 rot_mat(1.);
+    rot_mat = glm::rotate(rot_mat, camera_ang_xz, {0, 1, 0});
+    cam_pos = rot_mat * cam_pos;
+    gluLookAt(cam_pos[0], cam_pos[1], cam_pos[2], 0, 0, 0, 0, 1, 0);
+
 
     for (auto c : corners){
-        c.display();
+        c->display();
     }
 
     glPointSize(50.f);
@@ -162,72 +189,103 @@ void display() {
     glPopMatrix();
     glutSwapBuffers();
 
-    t += 1;
 }
 
-void keyboard(unsigned char key, int, int){
-    std::array<int, 4> swap = {0, 0, 0, 0};
-    int rotate = 0;
-    switch(key) {
-        case 'u':
-            swap = {3, 0, 1, 2};
+void special_input(int key, int x, int y){
+    switch(key){
+        case GLUT_KEY_UP:
+            camera_ang_y += _CAM_SPEED;
             break;
-        case 'U':
-            swap = {2, 1, 0, 3};
+        case GLUT_KEY_DOWN:
+            camera_ang_y -= _CAM_SPEED;
             break;
-        case 'd':
-            swap = {7, 4, 5, 6};
+        case GLUT_KEY_LEFT:
+            camera_ang_xz -= _CAM_SPEED;
             break;
-        case 'D':
-            swap = {6, 5, 4, 7};
-            break;
-        case 'f':
-            swap = {2, 1, 6, 5};
-            rotate = 1;
-            break;
-        case 'F':
-            swap = {5, 6, 1, 2};
-            rotate = -1;
-            break;
-        case 'b':
-            swap = {0, 3, 4, 7};
-            rotate = 1;
-            break;
-        case 'B':
-            swap = {7, 4, 3, 0};
-            rotate = -1;
-            break;
-        case 'r':
-            swap = {1, 0, 7, 6};
-            rotate = 1;
-            break;
-        case 'R':
-            swap = {6, 7, 0, 1};
-            rotate = -1;
-            break;
-        case 'l':
-            swap = {3, 2, 5, 4};
-            rotate = 1;
-            break;
-        case 'L':
-            swap = {4,5,2,3};
-            rotate = -1;
+        case GLUT_KEY_RIGHT:
+            camera_ang_xz += _CAM_SPEED;
             break;
 
         default: break;
     }
+}
 
-    corner_positions[swap[0]]->rotate(rotate);
-    corner_positions[swap[1]]->rotate(-rotate);
-    corner_positions[swap[2]]->rotate(rotate);
-    corner_positions[swap[3]]->rotate(-rotate);
+void start_animating_maybe(unsigned char key){
+    std::array<int, 4> swap = {0, 0, 0, 0};
+    glm::dvec3 axes;
+    switch(key) {
+        default: return;
 
-    auto temp = corner_positions[swap[3]];
+        case 'u':
+            swap = {3, 0, 1, 2};
+            axes = - move_axes[U];
+            break;
+        case 'U':
+            swap = {2, 1, 0, 3};
+            axes = move_axes[U];
+            break;
+        case 'd':
+            swap = {7, 4, 5, 6};
+            axes = - move_axes[D];
+            break;
+        case 'D':
+            swap = {6, 5, 4, 7};
+            axes = move_axes[D];
+            break;
+        case 'f':
+            swap = {2, 1, 6, 5};
+            axes = - move_axes[F];
+            break;
+        case 'F':
+            swap = {5, 6, 1, 2};
+            axes = move_axes[F];
+            break;
+        case 'b':
+            swap = {0, 3, 4, 7};
+            axes = - move_axes[B];
+            break;
+        case 'B':
+            swap = {7, 4, 3, 0};
+            axes = move_axes[B];
+            break;
+        case 'r':
+            swap = {1, 0, 7, 6};
+            axes = - move_axes[R];
+            break;
+        case 'R':
+            swap = {6, 7, 0, 1};
+            axes = move_axes[R];
+            break;
+        case 'l':
+            swap = {3, 2, 5, 4};
+            axes = - move_axes[L];
+            break;
+        case 'L':
+            swap = {4,5,2,3};
+            axes = move_axes[L];
+            break;
+    }
+
+    for (auto i : swap){
+        corner_positions[i]->animate(axes);
+    }
+    is_animating = true;
+    t = 0;
+
+    corner * temp = corner_positions[swap[3]];
     corner_positions[swap[3]] = corner_positions[swap[2]];
     corner_positions[swap[2]] = corner_positions[swap[1]];
     corner_positions[swap[1]] = corner_positions[swap[0]];
     corner_positions[swap[0]] = temp;
+
+}
+
+void keyboard(unsigned char key, int, int){
+    if(not is_animating){
+        start_animating_maybe(key);
+    }
     display();
+
 }
 
 void reshape(int w, int h){
@@ -244,35 +302,33 @@ void reshape(int w, int h){
     //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glViewport(0, 0, w, h);
     gluPerspective(_PERSPECTIVE, ratio, _NEAR, _FAR);
-    gluLookAt(0, 0, 0, 0, 0, -_CAM_DISTANCE, 0, 1, 0);
 
-    display();
 }
 
 void init_cube(){
     glm::dmat4 model(1.);
 
-    corners[0] = corner({COLOR_U, COLOR_B, COLOR_R}, model);
+    corners[0] = new corner({COLOR_U, COLOR_B, COLOR_R}, model);
     turn_face_mat(model, U);
-    corners[1] = corner({COLOR_U, COLOR_R, COLOR_F}, model);
+    corners[1] = new corner({COLOR_U, COLOR_R, COLOR_F}, model);
     turn_face_mat(model, U);
-    corners[2] = corner({COLOR_U, COLOR_F, COLOR_L}, model);
+    corners[2] = new corner({COLOR_U, COLOR_F, COLOR_L}, model);
     turn_face_mat(model, U);
-    corners[3] = corner({COLOR_U, COLOR_L, COLOR_B}, model);
+    corners[3] = new corner({COLOR_U, COLOR_L, COLOR_B}, model);
 
     model = glm::dmat4(1.);
     model = glm::rotate(model, -glm::pi<double>(), {0, 0, 1});
 
-    corners[4] = corner({COLOR_D, COLOR_B, COLOR_L}, model);
+    corners[4] = new corner({COLOR_D, COLOR_B, COLOR_L}, model);
     turn_face_mat(model, U);
-    corners[5] = corner({COLOR_D, COLOR_L, COLOR_F}, model);
+    corners[5] = new corner({COLOR_D, COLOR_L, COLOR_F}, model);
     turn_face_mat(model, U);
-    corners[6] = corner({COLOR_D, COLOR_F, COLOR_R}, model);
+    corners[6] = new corner({COLOR_D, COLOR_F, COLOR_R}, model);
     turn_face_mat(model, U);
-    corners[7] = corner({COLOR_D, COLOR_R, COLOR_B}, model);
+    corners[7] = new corner({COLOR_D, COLOR_R, COLOR_B}, model);
 
     for(int i = 0; i < corners.size(); ++i){
-        corner_positions[i] = &corners[i];
+        corner_positions[i] = corners[i];
     }
 }
 
@@ -288,10 +344,16 @@ int main(int argc, char** argv) {
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
+    glutSpecialFunc(special_input);
     glutReshapeFunc(reshape);
 
+    glutIdleFunc(display);
     reshape(800, 800);
     glutMainLoop();
+
+    for (auto c : corners){
+        delete c;
+    }
 
     return 0;
 }
